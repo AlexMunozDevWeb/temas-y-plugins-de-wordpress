9ef6380163c183d0dc14d31bb2ecf940ac896081
alex <alemumu207@gmail.com>
2023-03-16 22:11:57 +0100

    php

---
+++ php/includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP</title>
+    <link rel="stylesheet" href="dist/output.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body class="bg-slate-50">
+    
+    <div class="bg-blue-600 py-1"></div>
+    <div class="mx-auto max-w-7xl p-16 bg-white shadow-lg mt-10 rounded-lg text-2xl font-mono">
+        
