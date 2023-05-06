98e51175920d917481a62a9bfc20b4dec3cffde8
alex <alemumu207@gmail.com>
2023-05-06 10:59:59 +0200

    8. Generando el footer

--- wordpressGym/wp-content/themes/gym/footer.php
+++ wordpressGym/wp-content/themes/gym/footer.php
@@ -11,20 +11,20 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tema_vacio' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'tema_vacio' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tema_vacio' ), 'tema_vacio', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+	<footer class="footer contenedor">
+		<hr>
+		<div class="contenido-footer">
+			<?php 
+				//Se le pasa el menu que queremos que se muestre
+				$args = array(
+					'theme_location'  => 'menu-principal',
+					'container'			  => 'nav',
+					'container_class'	=> 'menu-principal',
+				);
+				wp_nav_menu( $args );
+			?>
+			<p class="copyright">Todos los derchos reservados. <?php echo get_bloginfo('name') . ' ' . date( 'Y' ) ?></p>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
